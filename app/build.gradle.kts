0f5c4e777a12a37a9d8cc9ebd2b07511400a2487
D4rK7355608 <d4rk7355608@gmail.com>
2024-05-28 09:05:40 +0300

    Made some small under-the-hood improvements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.d4rk.cartcalculator"
         minSdk = 26
         targetSdk = 34
-        versionCode = 37
-        versionName = "0.1.0"
+        versionCode = 38
+        versionName = "0.1.1"
         archivesName = "${applicationId}-v${versionName}"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations += listOf(
