8de1510b455cd1f2b333f32873177003913d0f83
D4rK7355608 <d4rk7355608@gmail.com>
2024-06-20 23:05:34 +0300

    Updated project props

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.d4rk.cartcalculator"
         minSdk = 26
         targetSdk = 34
-        versionCode = 45
-        versionName = "1.0.2"
+        versionCode = 46
+        versionName = "1.0.3"
         archivesName = "${applicationId}-v${versionName}"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations += listOf(
