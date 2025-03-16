43d2804e5507176fd9bcc3cee8f8301ea65f4312
Mihai-Cristian Condrea <mihai.condrea@digisoftware.ro>
2025-03-16 08:44:49 +0200

    feat: Updated KoinModule and removed hardcoded FAQs
    - Removed hardcoded FAQs from `KoinModule`.
    - Updated `KoinModule` to remove the `faqList` parameter when creating `AppInfo`.

--- app/src/main/java/com/d4rk/cartcalculator/core/di/KoinModule.kt
+++ app/src/main/java/com/d4rk/cartcalculator/core/di/KoinModule.kt
@@ -75,17 +75,7 @@ val appModule : Module = module {
             versionName = BuildConfig.VERSION_NAME ,
             versionCode = BuildConfig.VERSION_CODE ,
             appShortDescription = "Your app's short description" ,
-            faqList = listOf(
-                R.string.question_1 to R.string.summary_preference_faq_1,
-                R.string.question_2 to R.string.summary_preference_faq_2,
-                R.string.question_3 to R.string.summary_preference_faq_3,
-                R.string.question_4 to R.string.summary_preference_faq_4,
-                R.string.question_5 to R.string.summary_preference_faq_5,
-                R.string.question_6 to R.string.summary_preference_faq_6,
-                R.string.question_7 to R.string.summary_preference_faq_7,
-                R.string.question_8 to R.string.summary_preference_faq_8,
-                R.string.question_9 to R.string.summary_preference_faq_9
-            )
+
         )
     }
     single<GetFAQsUseCase> { GetFAQsUseCase(application = get()) }
