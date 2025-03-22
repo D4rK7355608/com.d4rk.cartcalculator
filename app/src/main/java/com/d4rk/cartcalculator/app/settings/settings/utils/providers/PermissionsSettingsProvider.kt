be110cc9174cd52fdeaabe2d745a686aedf2ecbe
Mihai-Cristian Condrea <mihai.condrea@digisoftware.ro>
2025-03-22 09:02:12 +0200

    feat: Improved Permissions Settings with string resources
    - Updated `PermissionsSettingsProvider` to use string resources for titles and summaries of settings preferences.
    - Added string resources in `R.string` for permissions like `ad_id`, `internet`, `post_notifications`, `access_network_state`, `access_notification_policy`, `billing`, `check_license`, and `foreground_service`.
    - Updated settings preferences titles and summaries to use context.getString to retrieve string resources.

--- app/src/main/java/com/d4rk/cartcalculator/app/settings/settings/utils/providers/PermissionsSettingsProvider.kt
+++ app/src/main/java/com/d4rk/cartcalculator/app/settings/settings/utils/providers/PermissionsSettingsProvider.kt
@@ -5,6 +5,7 @@ import com.d4rk.android.libs.apptoolkit.app.privacy.routes.permissions.utils.int
 import com.d4rk.android.libs.apptoolkit.app.settings.settings.domain.model.SettingsCategory
 import com.d4rk.android.libs.apptoolkit.app.settings.settings.domain.model.SettingsConfig
 import com.d4rk.android.libs.apptoolkit.app.settings.settings.domain.model.SettingsPreference
+import com.d4rk.cartcalculator.R
 
 class PermissionsSettingsProvider : PermissionsProvider {
     override fun providePermissionsConfig(context : Context) : SettingsConfig {
@@ -14,39 +15,40 @@ class PermissionsSettingsProvider : PermissionsProvider {
                     title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.normal) ,
                     preferences = listOf(
                         SettingsPreference(
-                            key = "ad_id" ,
-                            title = "sddsaddsadsa" ,
-                            summary = "dsdaddsad" ,
+                            title = context.getString(R.string.ad_id) ,
+                            summary = context.getString(R.string.summary_preference_permissions_ad_id) ,
                         ) ,
                         SettingsPreference(
-                            key = "ad_id" ,
-                            title = "sddsaddsadsa" ,
-                            summary = "dsdaddsad" ,
+                            title = context.getString(R.string.internet) ,
+                            summary = context.getString(R.string.summary_preference_permissions_internet) ,
                         ) ,
                         SettingsPreference(
-                            key = "ad_id" ,
-                            title = "sddsaddsadsa" ,
-                            summary = "dsdaddsad" ,
+                            title = context.getString(R.string.post_notifications) ,
+                            summary = context.getString(R.string.summary_preference_permissions_post_notifications) ,
                         ) ,
                     ) ,
-                ),
-                SettingsCategory(
+                ) , SettingsCategory(
                     title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.normal) ,
                     preferences = listOf(
                         SettingsPreference(
-                            key = "ad_id" ,
-                            title = "sddsaddsadsa" ,
-                            summary = "dsdaddsad" ,
+                            title = context.getString(R.string.access_network_state) ,
+                            summary = context.getString(R.string.summary_preference_permissions_access_network_state) ,
                         ) ,
                         SettingsPreference(
-                            key = "ad_id" ,
-                            title = "sddsaddsadsa" ,
-                            summary = "dsdaddsad" ,
+                            title = context.getString(R.string.access_notification_policy) ,
+                            summary = context.getString(R.string.summary_preference_permissions_access_notification_policy) ,
                         ) ,
                         SettingsPreference(
-                            key = "ad_id" ,
-                            title = "sddsaddsadsa" ,
-                            summary = "dsdaddsad" ,
+                            title = context.getString(R.string.billing) ,
+                            summary = context.getString(R.string.summary_preference_permissions_billing) ,
+                        ) ,
+                        SettingsPreference(
+                            title = context.getString(R.string.check_license) ,
+                            summary = context.getString(R.string.summary_preference_permissions_check_license) ,
+                        ) ,
+                        SettingsPreference(
+                            title = context.getString(R.string.foreground_service) ,
+                            summary = context.getString(R.string.summary_preference_permissions_foreground_service) ,
                         ) ,
                     ) ,
                 )
