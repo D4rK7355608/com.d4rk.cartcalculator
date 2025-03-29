f03bad77110769ffcf2ea56e98317aaadff52c6c
Mihai-Cristian Condrea <mihai.condrea@digisoftware.ro>
2025-03-29 16:01:05 +0200

    feat: Refactored AddCartItemUseCase for improved error handling
    - Updated `AddCartItemUseCase` to explicitly handle `ShoppingCartItemsTable` in `onSuccess` and `Throwable` in `onFailure` for clarity.
    - Modified `insertItem` call to explicitly specify the `item` parameter.

--- app/src/main/java/com/d4rk/cartcalculator/app/cart/domain/usecases/AddCartItemUseCase.kt
+++ app/src/main/java/com/d4rk/cartcalculator/app/cart/domain/usecases/AddCartItemUseCase.kt
@@ -12,11 +12,11 @@ import kotlinx.coroutines.flow.flow
 class AddCartItemUseCase(private val database : DatabaseInterface) : Repository<ShoppingCartItemsTable , Flow<DataState<ShoppingCartItemsTable , Errors>>> {
     override suspend fun invoke(param : ShoppingCartItemsTable) : Flow<DataState<ShoppingCartItemsTable , Errors>> = flow {
         runCatching {
-            val newItemId : Long = database.insertItem(param.copy(cartId = param.cartId))
+            val newItemId : Long = database.insertItem(item = param.copy(cartId = param.cartId))
             param.copy(itemId = newItemId.toInt() , cartId = param.cartId)
-        }.onSuccess { newItem ->
+        }.onSuccess { newItem : ShoppingCartItemsTable ->
             emit(value = DataState.Success(data = newItem))
-        }.onFailure { throwable ->
+        }.onFailure { throwable : Throwable ->
             emit(value = DataState.Error(error = throwable.toError(default = Errors.UseCase.FAILED_TO_ADD_CART_ITEM)))
         }
     }
