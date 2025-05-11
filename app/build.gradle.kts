f9fb13e69d924296af437cdba2f90ffbd4d19f81
Mihai-Cristian Condrea <mihai.condrea@digisoftware.ro>
2025-05-11 13:50:46 +0300

    feat: Update version code
    - Updated `versionCode` to `102`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.d4rk.cartcalculator"
         minSdk = 23
         targetSdk = 35
-        versionCode = 101
+        versionCode = 102
         versionName = "2.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         @Suppress("UnstableApiUsage") androidResources.localeFilters += listOf(
