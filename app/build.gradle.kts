15c39eab52eca5e0690e1058f361c9802232c1a9
D4rK7355608 <d4rk7355608@gmail.com>
2025-06-09 15:04:52 +0300

    feat: Add new translations for multiple languages
    - Added translations for Arabic (Egypt) (ar-rEG).
    - Added translations for Bengali (Bangladesh) (bn-rBD).
    - Added translations for Spanish (Mexico) (es-rMX).
    - Added translations for Filipino (Philippines) (fil-rPH).
    - Added translations for Korean (South Korea) (ko-rKR).
    - Added translations for Urdu (Pakistan) (ur-rPK).
    - Added translations for Vietnamese (Vietnam) (vi-rVN).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 23
         targetSdk = 36
         versionCode = 107
-        versionName = "2.0.1"
+        versionName = "2.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         @Suppress("UnstableApiUsage") androidResources.localeFilters += listOf(
             "ar-rEG" , "bg-rBG" , "bn-rBD" , "de-rDE" , "en" , "es-rGQ" , "es-rMX" , "fil-rPH" , "fr-rFR" , "hi-rIN" , "hu-rHU" , "in-rID" , "it-rIT" , "ja-rJP" , "ko-rKR" , "pl-rPL" , "pt-rBR" , "ro-rRO" , "ru-rRU" , "sv-rSE" , "th-rTH" , "tr-rTR" , "uk-rUA" , "ur-rPK" , "vi-rVN" , "zh-rTW"
