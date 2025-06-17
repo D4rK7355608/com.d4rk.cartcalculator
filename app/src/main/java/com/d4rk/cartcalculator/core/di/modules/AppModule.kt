a8e5f2d7cbda2e895a90eced12fb26aed5ae7f42
D4rK7355608 <d4rk7355608@gmail.com>
2025-06-17 09:17:40 +0300

    feat: Add Ktor client and search functionality
    - Added Ktor client to `AppModule`.
    - Implemented `SearchEventsUseCase` and `SearchViewModel` for event search functionality.
    - Integrated `SearchEventsUseCase` into `SearchViewModel` and provided it in `AppModule`.

--- app/src/main/java/com/d4rk/cartcalculator/core/di/modules/AppModule.kt
+++ app/src/main/java/com/d4rk/cartcalculator/core/di/modules/AppModule.kt
@@ -4,6 +4,7 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.IntentSenderRequest
 import com.d4rk.android.libs.apptoolkit.app.main.domain.usecases.PerformInAppUpdateUseCase
 import com.d4rk.android.libs.apptoolkit.app.oboarding.utils.interfaces.providers.OnboardingProvider
+import com.d4rk.android.libs.apptoolkit.data.client.KtorClient
 import com.d4rk.android.libs.apptoolkit.data.core.ads.AdsCoreManager
 import com.d4rk.cartcalculator.app.cart.details.domain.usecases.AddCartItemUseCase
 import com.d4rk.cartcalculator.app.cart.details.domain.usecases.DeleteCartItemUseCase
@@ -18,6 +19,8 @@ import com.d4rk.cartcalculator.app.cart.list.domain.usecases.ImportSharedCartUse
 import com.d4rk.cartcalculator.app.cart.list.domain.usecases.OpenCartUseCase
 import com.d4rk.cartcalculator.app.cart.list.domain.usecases.UpdateCartNameUseCase
 import com.d4rk.cartcalculator.app.cart.list.ui.HomeViewModel
+import com.d4rk.cartcalculator.app.cart.search.domain.usecases.SearchEventsUseCase
+import com.d4rk.cartcalculator.app.cart.search.ui.SearchViewModel
 import com.d4rk.cartcalculator.app.main.ui.MainViewModel
 import com.d4rk.cartcalculator.app.onboarding.utils.interfaces.providers.AppOnboardingProvider
 import com.d4rk.cartcalculator.core.data.datastore.DataStore
@@ -29,32 +32,57 @@ import org.koin.core.module.dsl.viewModel
 import org.koin.core.parameter.parametersOf
 import org.koin.dsl.module
 
-val appModule : Module = module {
+val appModule: Module = module {
     single<DataStore> { DataStore(context = get()) }
-    single<AdsCoreManager> { AdsCoreManager(context = get() , buildInfoProvider = get()) }
+    single<AdsCoreManager> { AdsCoreManager(context = get(), buildInfoProvider = get()) }
     single<AppUpdateManager> { AppUpdateManagerFactory.create(get()) }
+    single { KtorClient().createClient() }
 
     single<OnboardingProvider> { AppOnboardingProvider() }
 
-    factory { (launcher : ActivityResultLauncher<IntentSenderRequest>) ->
-        PerformInAppUpdateUseCase(appUpdateManager = get() , updateResultLauncher = launcher)
+    factory { (launcher: ActivityResultLauncher<IntentSenderRequest>) ->
+        PerformInAppUpdateUseCase(appUpdateManager = get(), updateResultLauncher = launcher)
     }
 
-    viewModel { (launcher : ActivityResultLauncher<IntentSenderRequest>) ->
-        MainViewModel(dispatcherProvider = get() , performInAppUpdateUseCase = get { parametersOf(launcher) })
+    viewModel { (launcher: ActivityResultLauncher<IntentSenderRequest>) ->
+        MainViewModel(
+            dispatcherProvider = get(),
+            performInAppUpdateUseCase = get { parametersOf(launcher) })
     }
 
     single<GetCartsUseCase> { GetCartsUseCase(database = get()) }
     single<DecryptSharedCartUseCase> { DecryptSharedCartUseCase() }
     single<AddCartUseCase> { AddCartUseCase(database = get()) }
     single<DeleteCartUseCase> { DeleteCartUseCase(database = get()) }
-    single<ImportSharedCartUseCase> { ImportSharedCartUseCase(database = get() , decryptSharedCartUseCase = get()) }
+    single<ImportSharedCartUseCase> {
+        ImportSharedCartUseCase(
+            database = get(),
+            decryptSharedCartUseCase = get()
+        )
+    }
     single<OpenCartUseCase> { OpenCartUseCase(context = get()) }
     single<GenerateCartShareLinkUseCase> { GenerateCartShareLinkUseCase(database = get()) }
     single<UpdateCartNameUseCase> { UpdateCartNameUseCase(database = get()) }
 
     viewModel {
-        HomeViewModel(getCartsUseCase = get() , addCartUseCase = get() , deleteCartUseCase = get() , importSharedCartUseCase = get() , openCartUseCase = get() , dispatcherProvider = get() , generateCartShareLinkUseCase = get() , updateCartNameUseCase = get() , dataStore = get())
+        HomeViewModel(
+            getCartsUseCase = get(),
+            addCartUseCase = get(),
+            deleteCartUseCase = get(),
+            importSharedCartUseCase = get(),
+            openCartUseCase = get(),
+            dispatcherProvider = get(),
+            generateCartShareLinkUseCase = get(),
+            updateCartNameUseCase = get(),
+            dataStore = get()
+        )
+    }
+
+    single<SearchEventsUseCase> { SearchEventsUseCase(database = get()) }
+    viewModel {
+        SearchViewModel(
+            searchEventsUseCase = get(), dispatcherProvider = get()
+        )
     }
 
     single<LoadCartUseCase> { LoadCartUseCase(database = get()) }
@@ -63,6 +91,14 @@ val appModule : Module = module {
     single<DeleteCartItemUseCase> { DeleteCartItemUseCase(database = get()) }
 
     viewModel {
-        CartViewModel(loadCartUseCase = get() , addCartItemUseCase = get() , updateCartItemUseCase = get() , deleteCartItemUseCase = get() , generateCartShareLinkUseCase = get() , dataStore = get() , dispatcherProvider = get())
+        CartViewModel(
+            loadCartUseCase = get(),
+            addCartItemUseCase = get(),
+            updateCartItemUseCase = get(),
+            deleteCartItemUseCase = get(),
+            generateCartShareLinkUseCase = get(),
+            dataStore = get(),
+            dispatcherProvider = get()
+        )
     }
 }
